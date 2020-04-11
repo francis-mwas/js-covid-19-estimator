6b28567e3a4c0f12df7bb3b1c1fceac89e2f43ac
francis-mwas <mwangif184@gmail.com>
2020-04-11 23:19:56 +0300

    Fixed months and weeks fails

--- src/estimator.js
+++ src/estimator.js
@@ -1,41 +1,27 @@
 const covid19ImpactEstimator = (data) => {
-  function convert(periodType) {
-    let toDays;
-    switch (periodType) {
-      case 'days':
-        toDays = data.timeToElapse;
-        break;
-      case 'weeks':
-        toDays = data.timeToElapse * 7;
-        break;
-      case 'months':
-        toDays = data.timeToElapse * 30;
-        break;
-      default:
-        toDays = data.timeToElapse;
+  function factor() {
+    let toDays = 0;
+    if (data.periodType === 'days') {
+      toDays = data.timeToElapse;
+    } else if (data.periodType === 'weeks') {
+      toDays = data.timeToElapse * 7;
+    } else if (data.periodType === 'months') {
+      toDays = data.timeToElapse * 30;
     }
 
-    return toDays;
-  }
-
-  function factor(timeToElapse) {
-    const how = convert(timeToElapse);
-
-    const num = how / 3;
-    const conv = Math.floor(num);
-    return conv;
+    return Math.trunc(toDays / 3);
   }
   // population,
   // totalHospitalBeds,
   // periodType,
   // region
-  const { timeToElapse, reportedCases } = data;
+  const { reportedCases } = data;
 
   const impactCurInfected = reportedCases * 10;
-  const ImpInfeByRequestedTime = impactCurInfected * 2 ** factor(timeToElapse);
+  const ImpInfeByRequestedTime = impactCurInfected * 2 ** factor();
 
   const severeCurInfected = reportedCases * 50;
-  const seInfeByRequestedTime = severeCurInfected * 2 ** factor(timeToElapse);
+  const seInfeByRequestedTime = severeCurInfected * 2 ** factor();
 
   const impact = {
     currentlyInfected: impactCurInfected,
